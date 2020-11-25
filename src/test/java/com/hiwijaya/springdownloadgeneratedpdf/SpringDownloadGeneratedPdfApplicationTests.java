d3e2fe3e2cbc3c540662536c1027b0d120ec5f1f
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-11-25 15:18:52 +0700

    set dependencies

---
+++ src/test/java/com/hiwijaya/springdownloadgeneratedpdf/SpringDownloadGeneratedPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiwijaya.springdownloadgeneratedpdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDownloadGeneratedPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
